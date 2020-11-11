4e9c69bdbce268991b79e4d23fd9628047385e0a
刘鑫 <liuxin@izaodao.org>
2020-11-11 23:06:07 +0800

    week03 homework 作业完成： 必做题1： 实现多种方式获取多线程执行结果
    必做题2：一张知识脑图

---
+++ Week_04/thursday/two.java
@@ -0,0 +1,51 @@
+package java0.conc0303.homework03;
+
+import java.util.Random;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+
+/**
+ * 本周作业：（必做）思考有多少种方式，在main函数启动一个新线程或线程池，
+ * 异步运行一个方法，拿到这个方法的返回值后，退出主线程？
+ * 写出你的方法，越多越好，提交到github。
+ * <p>
+ * 一个简单的代码参考：
+ */
+public class two {
+
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
+
+        long start = System.currentTimeMillis();
+        // 在这里创建一个线程或线程池，
+
+        ExecutorService executor = Executors.newCachedThreadPool();
+        Future<Integer> result = executor.submit(new Callable<Integer>() {
+            public Integer call() throws Exception {
+                return sum();
+            }
+        });
+        executor.shutdown();
+        // 异步执行 下面方法
+
+        // 确保  拿到result 并输出
+        System.out.println("异步计算结果为：" + result.get());
+
+        System.out.println("使用时间：" + (System.currentTimeMillis() - start) + " ms");
+
+        // 然后退出main线程
+    }
+
+    private static int sum() {
+        return fibo(36);
+    }
+
+    private static int fibo(int a) {
+        if (a < 2) {
+            return 1;
+        }
+        return fibo(a - 1) + fibo(a - 2);
+    }
+}
